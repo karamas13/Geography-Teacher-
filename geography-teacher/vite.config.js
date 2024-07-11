f70719ac0088a3b382b0f27b72cc1d80098d4fe8
karamas13 <99655065+karamas13@users.noreply.github.com>
2024-07-11 21:34:13 +0300

    Implemented google maps api and places api for autocomplete search

--- geography-teacher/vite.config.js
+++ geography-teacher/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_OPENAI_KEY': JSON.stringify(process.env.VITE_OPENAI_KEY),
+    'process.env.VITE_GOOGLE_MAPS_KEY': JSON.stringify(process.env.VITE_GOOGLE_MAPS_KEY),
+    'process.env.VITE_MAP_ID': JSON.stringify(process.env.VITE_MAP_ID),
+  }
 })
